d736eb3f45c43ce1850e7e1b1fc8191e21ceda9b
MasterJiraya005 <154448925+MasterJiraya005@users.noreply.github.com>
2024-01-10 11:18:07 +0300

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -18,7 +18,7 @@ async def save_group(bot, message):
         if not await db.get_chat(message.chat.id):
             total=await bot.get_chat_members_count(message.chat.id)
             r_j = message.from_user.mention if message.from_user else "Anonymous"
-            link = (await bot.create_chat_invite_link(chat_id)).invite_link
+            link = (await bot.create_chat_invite_link(message.chat.id)).invite_link
             await bot.send_message(LAZY_GROUP_LOGS, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, r_j, link))       
             await db.add_chat(message.chat.id, message.chat.title)
         if message.chat.id in temp.BANNED_CHATS:
