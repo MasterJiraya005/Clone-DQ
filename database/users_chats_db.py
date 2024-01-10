b7a7d80389b5e3a4414ae76d5268ae68591d74ab
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2024-01-10 15:11:26 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -27,6 +27,7 @@ class Database:
             title = title,
             chat_status=dict(
                 is_disabled=False,
+                is_lazy_verified=False,
                 reason="",
             ),
         )
@@ -95,9 +96,11 @@ class Database:
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
         chats = self.grp.find({'chat_status.is_disabled': True})
+        is_verified = self.grp.find({'chat_status.is_lazy_verified': True})
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
-        return b_users, b_chats
+        lz_verified = [chat['id'] async for chat in is_verified]
+        return b_users, b_chats, lz_verified
     
 
 
@@ -142,12 +145,19 @@ class Database:
         if chat:
             return chat.get('settings', default)
         return default
-    
+
 
     async def disable_chat(self, chat, reason="No Reason"):
         chat_status=dict(
             is_disabled=True,
             reason=reason,
+            )
+        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
+    
+
+    async def verify_lazy_chat(self, chat):
+        chat_status=dict(
+            is_lazy_verified=True,
             )
         await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
     
